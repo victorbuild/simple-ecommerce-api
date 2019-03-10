e6b9729f6442545c3b7bbcaf8367a20b5b7c8c7b
victor <alisw551@gmail.com>
2019-03-11 00:33:49 +0800

    Relationship created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,8 +3,9 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
 class Review extends Model
 {
-    //
+  public function product(){
+    return $this->belongsTo(Product::class);
+  }
 }
