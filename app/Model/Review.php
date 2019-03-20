919c19463647b639dd8e1413eb4e843bda369edd
victor <alisw551@gmail.com>
2019-03-20 15:20:38 +0800

    Review Created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -5,6 +5,10 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
+    protected $fillable = [
+        'star', 'customer', 'review'
+    ];
+
     public function product(){
         return $this->belongsTo(Product::class);
     }
